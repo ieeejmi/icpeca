3ab10a180de055b04e199688b5fe31d796ea58d1
ishannn05 <ishanhussain05@gmail.com>
2019-11-08 13:24:28 +0530

    FLANKS + CAROUSEL javascript piece included

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,20 @@
 
 $(function(){
-  $("#header").load("./header.html"); 
-});
-$(function(){
-  $("#leftFlank").load("./leftFlank.html"); 
-});
-$(function(){
-  $("#rightFlank").load("./rightFlank.html"); 
-});
-$(window).on('load', ()=>{
+  $("#header").load("./header.html",()=>{
    	var href = location.href;
 	var pgurl = href.substr(href.lastIndexOf('/') + 1);
 	// match all the anchors on the page with the html file name
    if(pgurl!=''){
     	$('a[href="' + pgurl + '"]').addClass('active');
-    	alert(pgurl);
 	}
 	else{
 		$('a[href="index.html"]').addClass('active');
 	}
+	});
+  }); 
+$(function(){
+  $("#leftFlank").load("./leftFlank.html"); 
+});
+$(function(){
+  $("#rightFlank").load("./rightFlank.html"); 
 });
