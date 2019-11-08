9225f7d37ac593a0a693bba04887f027de0d0a44
ishannn05 <ishanhussain05@gmail.com>
2019-11-08 12:55:20 +0530

    FLANKS + CAROUSEL javascript piece included

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+
+$(function(){
+  $("#header").load("./header.html"); 
+});
+$(function(){
+  $("#leftFlank").load("./leftFlank.html"); 
+});
+$(function(){
+  $("#rightFlank").load("./rightFlank.html"); 
+});
+$(window).on('load', ()=>{
+   	var href = location.href;
+	var pgurl = href.substr(href.lastIndexOf('/') + 1);
+	// match all the anchors on the page with the html file name
+   if(pgurl!=''){
+    	$('a[href="' + pgurl + '"]').addClass('active');
+	}
+	else{
+		$('a[href="index.html"]').addClass('active');
+	}
+});
